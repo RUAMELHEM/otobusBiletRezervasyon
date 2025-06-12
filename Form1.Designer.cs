4842ced80b7e5b4f3a4190cfaa8c895978c4988a
Rua MELHEM <153845904+RUAMELHEM@users.noreply.github.com>
2025-06-12 13:37:18 +0300

    Form1.Designer.cs

---
+++ Form1.Designer.cs
@@ -0,0 +1,260 @@
+namespace OtobusBiletRezervasyonu
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            panel1 = new Panel();
+            button5 = new Button();
+            button3 = new Button();
+            GoBus = new Label();
+            button1 = new Button();
+            bindingSource1 = new BindingSource(components);
+            button2 = new Button();
+            button6 = new Button();
+            button7 = new Button();
+            button8 = new Button();
+            button10 = new Button();
+            button11 = new Button();
+            button12 = new Button();
+            button13 = new Button();
+            button4 = new Button();
+            panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)bindingSource1).BeginInit();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.Silver;
+            panel1.Controls.Add(button5);
+            panel1.Controls.Add(button3);
+            panel1.Controls.Add(GoBus);
+            panel1.Controls.Add(button1);
+            panel1.Dock = DockStyle.Top;
+            panel1.Font = new Font("Segoe UI", 18F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            panel1.Location = new Point(0, 0);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(1057, 56);
+            panel1.TabIndex = 1;
+            // 
+            // button5
+            // 
+            button5.Font = new Font("Segoe UI Variable Display Semib", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button5.ForeColor = Color.RosyBrown;
+            button5.Location = new Point(849, 13);
+            button5.Name = "button5";
+            button5.Size = new Size(175, 35);
+            button5.TabIndex = 2;
+            button5.Text = "Çıkış";
+            button5.UseVisualStyleBackColor = true;
+            button5.Click += button5_Click;
+            // 
+            // button3
+            // 
+            button3.Font = new Font("Segoe UI Variable Display Semib", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button3.ForeColor = Color.RosyBrown;
+            button3.Location = new Point(619, 13);
+            button3.Name = "button3";
+            button3.Size = new Size(198, 35);
+            button3.TabIndex = 1;
+            button3.Text = "Şehir içi Sefer";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click_1;
+            // 
+            // GoBus
+            // 
+            GoBus.AutoSize = true;
+            GoBus.Font = new Font("Harlow Solid Italic", 25.8000011F, FontStyle.Italic, GraphicsUnit.Point, 0);
+            GoBus.ForeColor = SystemColors.ButtonHighlight;
+            GoBus.ImageAlign = ContentAlignment.TopCenter;
+            GoBus.Location = new Point(0, 1);
+            GoBus.Name = "GoBus";
+            GoBus.Size = new Size(257, 55);
+            GoBus.TabIndex = 0;
+            GoBus.Text = "YoldaKalma";
+            // 
+            // button1
+            // 
+            button1.Font = new Font("Segoe UI Variable Display Semib", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button1.ForeColor = Color.RosyBrown;
+            button1.Location = new Point(385, 12);
+            button1.Name = "button1";
+            button1.Size = new Size(198, 35);
+            button1.TabIndex = 0;
+            button1.Text = "Şehirler Arası Sefer";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.BackColor = Color.Silver;
+            button2.Image = Properties.Resources.Ekran_görüntüsü_2025_05_28_172350;
+            button2.Location = new Point(54, 214);
+            button2.Name = "button2";
+            button2.Size = new Size(160, 158);
+            button2.TabIndex = 2;
+            button2.UseVisualStyleBackColor = false;
+            button2.Click += button2_Click_1;
+            // 
+            // button6
+            // 
+            button6.BackColor = Color.LightGray;
+            button6.Image = (Image)resources.GetObject("button6.Image");
+            button6.Location = new Point(602, 214);
+            button6.Name = "button6";
+            button6.Size = new Size(167, 166);
+            button6.TabIndex = 3;
+            button6.UseVisualStyleBackColor = false;
+            // 
+            // button7
+            // 
+            button7.BackColor = Color.Silver;
+            button7.Image = Properties.Resources.Ekran_görüntüsü_2025_05_28_175438;
+            button7.Location = new Point(857, 217);
+            button7.Name = "button7";
+            button7.Size = new Size(167, 163);
+            button7.TabIndex = 4;
+            button7.UseVisualStyleBackColor = false;
+            // 
+            // button8
+            // 
+            button8.BackColor = Color.LightGray;
+            button8.Image = (Image)resources.GetObject("button8.Image");
+            button8.Location = new Point(340, 214);
+            button8.Name = "button8";
+            button8.Size = new Size(164, 164);
+            button8.TabIndex = 5;
+            button8.UseVisualStyleBackColor = false;
+            // 
+            // button10
+            // 
+            button10.BackColor = Color.LightGray;
+            button10.Font = new Font("Segoe UI Variable Display Semib", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button10.ForeColor = Color.RosyBrown;
+            button10.Location = new Point(10, 162);
+            button10.Name = "button10";
+            button10.Size = new Size(247, 57);
+            button10.TabIndex = 10;
+            button10.Text = "7/24 Müşteri Hizmetleri";
+            button10.UseVisualStyleBackColor = false;
+            button10.Click += button10_Click;
+            // 
+            // button11
+            // 
+            button11.BackColor = Color.LightGray;
+            button11.Font = new Font("Segoe UI Variable Display Semib", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button11.ForeColor = Color.RosyBrown;
+            button11.Location = new Point(565, 162);
+            button11.Name = "button11";
+            button11.Size = new Size(233, 57);
+            button11.TabIndex = 11;
+            button11.Text = "Seçkin Firmalar";
+            button11.UseVisualStyleBackColor = false;
+            button11.Click += button11_Click;
+            // 
+            // button12
+            // 
+            button12.BackColor = Color.LightGray;
+            button12.Font = new Font("Segoe UI Variable Display Semib", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button12.ForeColor = Color.RosyBrown;
+            button12.Location = new Point(-88, -77);
+            button12.Name = "button12";
+            button12.Size = new Size(247, 57);
+            button12.TabIndex = 12;
+            button12.Text = "Güvenli Ödeme";
+            button12.UseVisualStyleBackColor = false;
+            // 
+            // button13
+            // 
+            button13.BackColor = Color.LightGray;
+            button13.Font = new Font("Segoe UI Variable Display Semib", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button13.ForeColor = Color.RosyBrown;
+            button13.Location = new Point(296, 162);
+            button13.Name = "button13";
+            button13.Size = new Size(244, 57);
+            button13.TabIndex = 13;
+            button13.Text = "Bütçe Dostu";
+            button13.UseVisualStyleBackColor = false;
+            button13.Click += button13_Click;
+            // 
+            // button4
+            // 
+            button4.BackColor = Color.LightGray;
+            button4.Font = new Font("Segoe UI Variable Display Semib", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button4.ForeColor = Color.RosyBrown;
+            button4.Location = new Point(818, 162);
+            button4.Name = "button4";
+            button4.Size = new Size(227, 57);
+            button4.TabIndex = 14;
+            button4.Text = "Güvenli";
+            button4.UseVisualStyleBackColor = false;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.DimGray;
+            ClientSize = new Size(1057, 506);
+            Controls.Add(button4);
+            Controls.Add(button13);
+            Controls.Add(button12);
+            Controls.Add(button11);
+            Controls.Add(button10);
+            Controls.Add(button8);
+            Controls.Add(button7);
+            Controls.Add(button6);
+            Controls.Add(button2);
+            Controls.Add(panel1);
+            Name = "Form1";
+            Text = "Otobüs Bilet Rezervasyonu Sitesi";
+            Load += Form1_Load;
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)bindingSource1).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+        private Panel panel1;
+        private Label GoBus;
+        private BindingSource bindingSource1;
+        private Button button1;
+        private Button button2;
+        private Button button6;
+        private Button button7;
+        private Button button8;
+        private Button button5;
+        private Button button10;
+        private Button button11;
+        private Button button12;
+        private Button button13;
+        private Button button4;
+        private Button button3;
+    }
+}
